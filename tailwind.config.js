a740bde662e4c0019f32769843015633bb480c0d
Luis Angel <angel.quispe.navarro@outlook.com>
2025-04-02 22:07:01 -0500

    Configuracion y aplicacion de estilos iniciales

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+const config = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
+export default config;
